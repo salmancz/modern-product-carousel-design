c24c1fd5a6fe4f91c7ec55eac994261ebf7e7d8e
salmancz <salman.jsdev@gmail.com>
2022-08-20 20:56:21 +0530

    modified:   app.js 	modified:   style.css

---
+++ app.js
@@ -0,0 +1,5 @@
+let navLinks = document.querySelectorAll(".carousel .nav-link");
+let slides = document.querySelectorAll(".carousel .slides img");
+let overlays = document.querySelectorAll(".carousel .bar");
+let maxZIndex = navLinks.length;
+let easeInOutQuart = "cubic-bezier(0.77, 0, 0.175, 1)";
